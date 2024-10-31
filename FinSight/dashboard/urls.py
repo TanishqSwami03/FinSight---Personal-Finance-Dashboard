13653613ccb1c1769555bfaa3d93cbc74006b64d
TanishqSwami03 <tanishqrajswami@gmail.com>
2024-10-31 11:14:45 +0530

    few additions and some changes

--- FinSight/dashboard/urls.py
+++ FinSight/dashboard/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
+    path('dashboard/transaction/', transaction, name='transaction'),
+    path('dashboard/portfolio/', portfolio, name='portfolio'),
+    path('dashboard/wallet/', wallet, name='wallet'),
+    path('dashboard/calculator/', calculator, name='calculator'),
 ]
--- FinSight/dashboard/views.py
+++ FinSight/dashboard/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def transaction(request):
+    return render(request, 'transaction.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def wallet(request):
+    return render(request, 'wallet.html')
+
+def calculator(request):
+    return render(request, 'calculator.html')
